77158485a9c54729d265f1c8024c11a88bec290b
M MOHAMED SILAR <143829598+MohamedSilar@users.noreply.github.com>
2025-07-20 10:52:57 +0530

    Time: 18 ms (15.23%) | Memory: 44.6 MB (42.86%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -9,24 +9,36 @@ class Solution {
            return Arrays.equals(sarray,tarray);
 */
            // optimal Solution 
-            
+            // if(s.length() != t.length()) return false;
+            // char[] chararray = new char[26];
+            // for(int i = 0 ;i<s.length();i++){
+            //     chararray[s.charAt(i) - 'a'] ++;
+            //     chararray[t.charAt(i) - 'a'] --;
+            // }
 
-            if(s.length() != t.length()) return false;
+            // for(int count : chararray){
+            //     if(count != 0) return false;
+            // }
+            // return true;
+
+            if (s.length() != t.length()) return false;
 
 
-            char[] chararray = new char[26];
+            Map<Character, Integer> maps = new HashMap<>();
 
-            for(int i = 0 ;i<s.length();i++){
-                chararray[s.charAt(i) - 'a'] ++;
-                chararray[t.charAt(i) - 'a'] --;
+            for(int i=0;i<s.length();i++){
+                maps.put(s.charAt(i) ,maps.getOrDefault(s.charAt(i),0) +1);
             }
 
-            for(int count : chararray){
-                if(count != 0) return false;
+            for(int i = 0 ;i<t.length();i++){
+                if(maps.containsKey(t.charAt(i)) && maps.get(t.charAt(i))>0){
+                    maps.put(t.charAt(i) , maps.get(t.charAt(i))-1);
+                   
+                }else{
+                     return false;
+                }
             }
 
             return true;
-
-        
     }
 }
