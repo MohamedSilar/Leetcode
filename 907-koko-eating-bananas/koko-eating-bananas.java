a5cbd138cc7c6041f83b93224e29642d7743363b
M MOHAMED SILAR <143829598+MohamedSilar@users.noreply.github.com>
2025-07-23 06:50:26 +0530

    Time: 25 ms (39.21%) | Memory: 45.6 MB (15.49%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+
+        int n = piles.length;
+
+        int left = 1  , right = getmax(piles);
+
+        while(left < right){
+            int mid = left + (right - left) / 2;
+            if(caneatall(piles , h , mid)){
+                   right = mid;
+            }else{
+                 left = mid + 1;
+            }
+        }
+        return left;
+    }
+    private static int getmax(int[] piles){
+        int max = 0;
+
+        for(int pile : piles){
+            if(pile > max){
+                max = pile;
+            }
+        }
+        return max;
+    }
+
+    private static boolean caneatall(int[] piles , int h , int mid){
+            int hours = 0;
+        for(int bananas : piles){
+                hours+= Math.ceil((double) bananas/ mid);
+            }
+
+            return hours<=h;
+    }
+}
