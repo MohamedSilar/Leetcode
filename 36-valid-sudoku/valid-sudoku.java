e2d347aa38c66660adf1514cd37acffa56ee4225
M MOHAMED SILAR <143829598+MohamedSilar@users.noreply.github.com>
2025-08-20 10:21:28 +0530

    Time: 14 ms (15.25%) | Memory: 45.2 MB (11.57%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet seen = new HashSet<>();
+        for(int i =0;i<9;i++){
+            for(int j=0;j<9;j++){
+                char current = board[i][j];
+                if(current !='.'){
+                    String row = current + " in row "+i;
+                     String col = current + " in colum"+j;
+                      String box = current +" in box" + i/3+ " - " + j/3;
+                
+                if(!seen.add(row) || !seen.add(col) || !seen.add(box)) {
+                return false;
+                }
+                }
+            }
+        }
+        return true;
+
+        
+    }
+}
