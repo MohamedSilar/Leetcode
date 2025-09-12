91d96fa2a64f7a03973fb7308e28a9dec3e62105
M MOHAMED SILAR <143829598+MohamedSilar@users.noreply.github.com>
2025-09-12 13:57:52 +0530

    Time: 0 ms (100.00%) | Memory: 57.7 MB (14.82%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,18 +1,15 @@
 class Solution {
     public void rotate(int[] nums, int k) {
+        int n = nums.length;
 
-        int n =nums.length;
+        k = k % n;
 
-         k = k % n;
-
-        reverse(nums , 0 , n-1);
-        reverse(nums , 0 , k-1);
-        reverse(nums , k , n-1);
-
-
+        reverse(nums,0,n-1);
+        reverse(nums,0,k-1);
+        reverse(nums,k,n-1);
     }
         private static void reverse(int[] nums , int left , int right){
-            while(left <= right){
+            while(left<=right){
                 int temp = nums[left];
                 nums[left] = nums[right];
                 nums[right] = temp;
@@ -20,5 +17,26 @@ class Solution {
                 right--;
             }
         }
-    }
+}
+
+    //     int n =nums.length;
+
+    //      k = k % n;
+
+    //     reverse(nums , 0 , n-1);
+    //     reverse(nums , 0 , k-1);
+    //     reverse(nums , k , n-1);
+
+
+    // }
+    //     private static void reverse(int[] nums , int left , int right){
+    //         while(left <= right){
+    //             int temp = nums[left];
+    //             nums[left] = nums[right];
+    //             nums[right] = temp;
+    //             left++;
+    //             right--;
+    //         }
+    //     }
+    // }
 
