b7234876295756055540ed5a41273b136f7433f5
M MOHAMED SILAR <143829598+MohamedSilar@users.noreply.github.com>
2025-09-25 11:13:15 +0530

    Time: 5 ms (58.00%) | Memory: 44.6 MB (84.47%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,22 +1,29 @@
-class Solution {
-    public int romanToInt(String s) {
-        Map<Character,Integer> m = new HashMap<>();
-        m.put('I',1);
-        m.put('V',5);
-        m.put('X',10);
-        m.put('L',50);
-        m.put('C',100);
-        m.put('D',500);
-        m.put('M',1000);
+import java.util.HashMap;
 
-        int ans = 0;
-        for(int i =0 ;i<s.length();i++){
-            if(i<s.length() -1 && m.get(s.charAt(i)) < m.get(s.charAt(i+1))){
-                ans -= m.get(s.charAt(i));
-            }else{
-                ans += m.get(s.charAt(i));
+public class Solution {
+    public int romanToInt(String s) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int result = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            int value = map.get(s.charAt(i));
+
+            // If the current value is less than the next one, subtract it
+            if (i + 1 < s.length() && value < map.get(s.charAt(i + 1))) {
+                result -= value;
+            } else {
+                result += value;
             }
         }
-        return ans;
+
+        return result;
     }
 }
