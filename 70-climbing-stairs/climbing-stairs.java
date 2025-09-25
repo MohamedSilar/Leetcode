95b5fa4339838cb0e713c0222c71d94ade4d7f4a
M MOHAMED SILAR <143829598+MohamedSilar@users.noreply.github.com>
2025-09-25 11:20:37 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (28.56%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int climbStairs(int n) {
+        if (n <= 2) return n;
+
+        int first = 1;  // ways to reach step 1
+        int second = 2; // ways to reach step 2
+
+        for (int i = 3; i <= n; i++) {
+            int third = first + second; // ways to reach current step
+            first = second;
+            second = third;
+        }
+
+        return second;
+    }
+}
