844cb93b4c6a53729543b7772dfd56a4a8df3b69
M MOHAMED SILAR <143829598+MohamedSilar@users.noreply.github.com>
2025-12-24 11:46:43 +0530

    Time: 2 ms (95.09%) | Memory: 45 MB (93.91%) - LeetSync

---
+++ 44-wildcard-matching/wildcard-matching.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int i =0,j=0;
+        int start =-1,match =0;
+        while(i < s.length()){
+            if(j < p.length() && (p.charAt(j) == '?' || s.charAt(i) == p.charAt(j))){
+                i++;
+                j++;
+            }else if(j < p.length() && p.charAt(j) == '*'){
+                start = j;
+                match = i;
+                j++;
+            }else if(start != -1){
+                j = start + 1;
+                match++;
+                i = match;
+            }else{
+                return false;
+            }
+        }
+        while(j < p.length() && p.charAt(j) == '*'){
+
+            j++;
+        }
+      return  j == p.length();
+    }
+}
